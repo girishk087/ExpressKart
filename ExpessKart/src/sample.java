422df964b396eee0fbefec0a255e3e9401e175d4
girishk087 <girishk087@gmail.com>
2015-07-04 20:40:22 +0530

    commiting

--- ExpessKart/src/sample.java
+++ ExpessKart/src/sample.java
@@ -3,5 +3,6 @@ public class sample {
 public static void main(String[] args) {
 	System.out.println("ExpressKart");
 	System.out.println("welcome to ExpressKart");
+	System.out.println("Fast way to buy");
 }
 }
