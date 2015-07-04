97595437eea44e43ba889fcade752d210bb46afd
Manjunath M <mmanjunath998@gmail.com>
2015-07-04 20:41:11 +0530

    from manju

--- ExpessKart/src/sample.java
+++ ExpessKart/src/sample.java
@@ -4,5 +4,6 @@ public static void main(String[] args) {
 	System.out.println("ExpressKart");
 	System.out.println("welcome to ExpressKart");
 	System.out.println("Fast way to buy");
+	System.out.println("from manju");
 }
 }
