90419f735791e6974bb046358a37a28c4e033795
girishk087 <girishk087@gmail.com>
2015-07-04 20:45:42 +0530

    all is well

--- ExpessKart/src/sample.java
+++ ExpessKart/src/sample.java
@@ -1,10 +1,6 @@
 
 public class sample {
 public static void main(String[] args) {
-	System.out.println("ExpressKart");
-	System.out.println("welcome to ExpressKart");
-	System.out.println("Fast way to buy");
-	System.out.println("from manju");
-	System.out.println("from giri");
+	System.out.println("dont worry everything is fine");
 }
 }
